c67d537ab20913b532c01c15b74c96529aa1df95
皓东haocity <ureygt@gmail.com>
2016-12-12 20:33:24 +0800

    修改post文章页面图标

--- post.php
+++ post.php
@@ -4,11 +4,12 @@
 <div class="main-left" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
-        <ul class="post-meta">
-            <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-            <li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
-            <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-        </ul>
+  			<ul class="post-meta">
+				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('<i class="iconfont icon-account"></i> '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+				<li><?php _e('<i class="iconfont icon-date"></i> '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
+				<li><?php _e('<i class="iconfont icon-tubiao3"></i>'); ?><?php $this->category(','); ?></li>
+				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><i class="iconfont icon-pinglun"></i><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+			</ul>
         <div class="post-content" itemprop="articleBody">
             <?php $this->content(); ?>
         </div>
